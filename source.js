fd5086c02893c1e7280e52bddb5d4142553ec982
Lohen Chen <64998077+Lohen-Chen@users.noreply.github.com>
2022-11-28 13:45:59 -0800

    Create source.js

---
+++ source.js
@@ -0,0 +1,1 @@
+(function(){ username = document.getElementById('portalAccountUsername'); username.value = "Enter Email Here"; document.getElementById("next").click(); document.getElementById("portalAccountPassword").value = "Enter Password Here"; document.getElementById("LoginButton").click();})()
